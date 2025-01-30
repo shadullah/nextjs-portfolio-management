61c9c7afdc5b8197343df94f109578573fa471c7
hexaalif <shadullahsad9@gmail.com>
2025-01-31 00:48:07 +0600

    project details page designed

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
@@ -50,7 +51,7 @@ const ProjectDetails = () => {
   }
 
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <div className="px-6 md:px-0 mt-24">
         <h1 className="font-syne text-4xl md:text-7xl font-bold mb-3">
           Project Details
@@ -60,36 +61,24 @@ const ProjectDetails = () => {
         </p>
         <div className="border-[1px] border-white mb-24"></div>
       </div>
-      <div>
-        <h1 className="font-syne text-4xl md:text-7xl font-bold mb-3">
-          {project?.title}
-        </h1>{" "}
-        <div className="px-6 md:px-0 mt-24">
-          <p className="font-rubik text-sm md:text-lg font-medium text-[#949494] mb-14 md:mb-20">
+      <div className="mb-48">
+        {project?.images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            height={636}
+            width={1150}
+            alt={`Project Image ${index + 1}`}
+            className="w-full h-auto rounded-lg"
+          />
+        ))}
+        <div className="mx-36">
+          <h1 className="font-syne text-3xl font-bold mb-14 my-24">
+            Project Story
+          </h1>{" "}
+          <p className="font-rubik text-sm md:text-lg text-[#9CA7B8] mb-14 md:mb-20">
             {project?.description}
           </p>
-          <div className="border-[1px] border-white mb-24"></div>
-          <div>
-            <h2 className="font-bold text-2xl mb-4">Client</h2>
-            <p>{project?.client}</p>
-            <h2 className="font-bold text-2xl mt-8 mb-4">Work</h2>
-            <ul>
-              {project?.work.map((task, index) => (
-                <li key={index}>{task}</li>
-              ))}
-            </ul>
-            <h2 className="font-bold text-2xl mt-8 mb-4">Images</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {project?.images.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`Project Image ${index + 1}`}
-                  className="w-full h-auto rounded-lg"
-                />
-              ))}
-            </div>
-          </div>
         </div>
       </div>
     </div>
